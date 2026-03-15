3ab11e20d1ab8b55e7a6dcf15aab96a6d4159234
Yash Yadav <yashydvv07@gmail.com>
2026-03-15 19:44:35 +0530

    COMPLETED

--- DAY43/code43.c
+++ DAY43/code43.c
@@ -1,3 +1,20 @@
+/*Problem Statement:
+Construct a Binary Tree from the given level-order traversal.
+
+Input Format:
+- First line contains integer N
+- Second line contains N space-separated integers (-1 represents NULL)
+
+Output Format:
+- Print inorder traversal of the constructed tree
+
+Example:
+Input:
+7
+1 2 3 4 5 -1 6
+
+Output:
+4 2 5 1 3 6*/
 #include <stdio.h>
 #include <stdlib.h>
 
