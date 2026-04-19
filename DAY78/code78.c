f53f82535d8b0effe1e4dd3eea55ff202d3f73b0
Yash Yadav <yashydvv07@gmail.com>
2026-04-19 16:31:07 +0530

    COMPLETED

---
+++ DAY78/code78.c
@@ -0,0 +1,93 @@
+/*Problem Statement
+Given weighted undirected graph, compute total weight of Minimum Spanning Tree using Prim’s algorithm.
+
+Input Format
+n m
+u v w
+...
+
+Output Format
+Total weight of MST.
+
+Sample Input
+4 5
+1 2 3
+1 3 5
+2 3 1
+2 4 4
+3 4 2
+
+Sample Output
+6
+
+Explanation
+One possible MST edges: (2-3), (3-4), (1-2)*/
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 100
+
+int minKey(int key[], int mstSet[], int n) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < n; v++) {
+        if (mstSet[v] == 0 && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+    return min_index;
+}
+
+int primMST(int graph[MAX][MAX], int n) {
+    int parent[n];
+    int key[n];
+    int mstSet[n];
+
+    for (int i = 0; i < n; i++) {
+        key[i] = INT_MAX;
+        mstSet[i] = 0;
+    }
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < n - 1; count++) {
+        int u = minKey(key, mstSet, n);
+        mstSet[u] = 1;
+
+        for (int v = 0; v < n; v++) {
+            if (graph[u][v] && mstSet[v] == 0 && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    int total = 0;
+    for (int i = 1; i < n; i++) {
+        total += graph[i][parent[i]];
+    }
+
+    return total;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int graph[MAX][MAX] = {0};
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        u--; v--;
+        graph[u][v] = w;
+        graph[v][u] = w;
+    }
+
+    int result = primMST(graph, n);
+    printf("%d\n", result);
+
+    return 0;
+}
